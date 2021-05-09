98e389adcb2e71a4af93113cbef366e96c7ec112
riyadhswe <riyadhswe@gmail.com>
2021-05-09 22:11:22 +0600

    ITShop

---
+++ store/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models.product import Product
+from .models.category import Category
+from .models.customer import Customer
+from .models.orders import Order
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name']
+
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ['first_name','phone','email']
+
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['product','customer','quantity','price','address','phone','date','status',]
+
+
+
+
+# Register your models here.
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category , AdminCategory)
+admin.site.register(Customer, AdminCustomer)
+admin.site.register(Order , AdminOrder)
